6ccd4787b755902739e2e2eb066bb04b4d872596
LAVgame <vipaleksei007@gmail.com>
2023-09-11 12:09:52 +0300

    one commit

---
+++ Lab_2/2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Калькулятор простых операций");
+            Console.WriteLine("Введите число А");
+            float a = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Введите число Б");
+            float b = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine($"A + B = {a+b}");
+            Console.WriteLine($"A - B = {a-b}");
+            Console.WriteLine($"A x B = {a*b}");
+            Console.WriteLine($"A : B = {a/b:0.00}");
+            Console.ReadLine();
+        }
+    }
+}
